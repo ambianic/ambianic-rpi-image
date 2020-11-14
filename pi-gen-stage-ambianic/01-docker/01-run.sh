3849496d65c3bf6c16ff08fa67115548b7bb0e99
Ivelin Ivanov <ivelin@ambianic.ai>
2020-11-14 13:49:02 -0600

    feat: docker files install script [skip ci]

---
+++ pi-gen-stage-ambianic/01-docker/01-run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e 
+
+#
+# Install docker engine and ambianic edge images
+#
+
+INSTALLDIR=/opt/ambianic
+BRANCH=main
+TMPDIR=$(dirname $(mktemp tmp.XXXXXXXXXX -ut))
+
+sudo true
+
+if ! type "git" > /dev/null; then
+    sudo apt update -q && sudo apt install git -y
+fi
+
+echo "Installing Ambianic.ai in $INSTALLDIR"
+
+# clean install
+echo "Installing in $INSTALLDIR"
+git clone -b $BRANCH https://github.com/ambianic/ambianic-quickstart.git $TMPDIR/ambianic
+sudo mv $TMPDIR/ambianic $INSTALLDIR
+sh $INSTALLDIR/scripts/setup.sh
