951030a6d610b7739fc9af5c2317698be3341a37
Ivelin Ivanov <ivelin@ambianic.ai>
2020-11-15 02:29:32 -0600

    fix: escape var expansion

--- pi-gen-stage-ambianic/01-docker/01-run.sh
+++ pi-gen-stage-ambianic/01-docker/01-run.sh
@@ -8,11 +8,11 @@
 on_chroot << EOF
 # set bash flags: e - fail on unset vars, x - verbose, u - fail quick 
 set -exu 
-export INSTALLDIR="/opt/ambianic"
-echo "$INSTALLDIR"
-mkdir -p "$INSTALLDIR"
+INSTALLDIR="/opt/ambianic"
+echo "\$INSTALLDIR"
+mkdir -p "\$INSTALLDIR"
 BRANCH=master
-TMPDIR="$(dirname $(mktemp tmp.XXXXXXXXXX -ut))"
+TMPDIR="\$(dirname \$(mktemp tmp.XXXXXXXXXX -ut))"
 
 sudo true
 
@@ -20,14 +20,14 @@ if ! type "git" > /dev/null; then
     sudo apt update -q && sudo apt install git -y
 fi
 
-echo "Installing Ambianic.ai in $INSTALLDIR"
+echo "Installing Ambianic.ai in \$INSTALLDIR"
 
 # clean install
-echo "Installing in $INSTALLDIR"
-mkdir -p "$TMPDIR/ambianic"
-git clone -b "$BRANCH" "https://github.com/ambianic/ambianic-quickstart.git" "$TMPDIR/ambianic"
-sudo mv "$TMPDIR/ambianic" "$INSTALLDIR"
-sh "$INSTALLDIR/scripts/setup.sh"
+echo "Installing in \$INSTALLDIR"
+mkdir -p "\$TMPDIR/ambianic"
+git clone -b "\$BRANCH" "https://github.com/ambianic/ambianic-quickstart.git" "\$TMPDIR/ambianic"
+sudo mv "\$TMPDIR/ambianic" "\$INSTALLDIR"
+sh "\$INSTALLDIR/scripts/setup.sh"
 
 EOF
 
