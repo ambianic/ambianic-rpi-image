e5818e5f234c7abe4cb3542694d1d13886224ee7
Ivelin Ivanov <ivelin@ambianic.ai>
2020-11-15 02:09:42 -0600

    fix: var scope

--- pi-gen-stage-ambianic/01-docker/01-run.sh
+++ pi-gen-stage-ambianic/01-docker/01-run.sh
@@ -8,11 +8,11 @@
 on_chroot << EOF
 # set bash flags: e - fail on unset vars, x - verbose, u - fail quick 
 set -exu 
-export INSTALLDIR=/opt/ambianic
-echo $INSTALLDIR
-mkdir -p $INSTALLDIR
+export INSTALLDIR="/opt/ambianic"
+echo "$INSTALLDIR"
+mkdir -p "$INSTALLDIR"
 BRANCH=master
-TMPDIR=$(dirname $(mktemp tmp.XXXXXXXXXX -ut))
+TMPDIR="$(dirname $(mktemp tmp.XXXXXXXXXX -ut))"
 
 sudo true
 
@@ -24,10 +24,10 @@ echo "Installing Ambianic.ai in $INSTALLDIR"
 
 # clean install
 echo "Installing in $INSTALLDIR"
-mkdir -p $TMPDIR/ambianic
-git clone -b $BRANCH https://github.com/ambianic/ambianic-quickstart.git $TMPDIR/ambianic
-sudo mv $TMPDIR/ambianic $INSTALLDIR
-sh $INSTALLDIR/scripts/setup.sh
+mkdir -p "$TMPDIR/ambianic"
+git clone -b "$BRANCH" "https://github.com/ambianic/ambianic-quickstart.git" "$TMPDIR/ambianic"
+sudo mv "$TMPDIR/ambianic" "$INSTALLDIR"
+sh "$INSTALLDIR/scripts/setup.sh"
 
 EOF
 
