bf265b5c953ab4c41fd9ee4584c8660f7a05b3cf
Ivelin Ivanov <ivelin@ambianic.ai>
2020-11-16 21:53:35 -0600

    fix: enable docker compose on boot

--- pi-gen-stage-ambianic/01-docker/02-run.sh
+++ pi-gen-stage-ambianic/01-docker/02-run.sh
@@ -7,3 +7,5 @@
 # Setup docker-compose config for boot service 
 install -m 644 files/docker-compose-app.service "${ROOTFS_DIR}/etc/systemd/system/docker-compose-app.service"
 
+# enable docker compose on boot
+systemctl enable docker-compose-app
