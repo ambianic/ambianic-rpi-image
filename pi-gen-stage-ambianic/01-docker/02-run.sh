467ae6c1896121d7396b6de9e65be9cb1b31681b
Ivelin Ivanov <ivelin@ambianic.ai>
2020-11-16 21:45:19 -0600

    fix: docker-compose boot time script

---
+++ pi-gen-stage-ambianic/01-docker/02-run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+#
+# Install default config files on target image
+#
+
+# Setup docker-compose config for boot service 
+install -m 644 files/docker-compose-app.service "${ROOTFS_DIR}/etc/systemd/system/docker-compose-app.service"
+
