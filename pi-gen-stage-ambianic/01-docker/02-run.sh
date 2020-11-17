62245ef10e06472ba15e8b37f9e99b0e3a2cc1d3
Ivelin Ivanov <ivelin@ambianic.ai>
2020-11-17 00:25:32 -0600

    fix: install docker-compose

--- pi-gen-stage-ambianic/01-docker/02-run.sh
+++ pi-gen-stage-ambianic/01-docker/02-run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -e
 
 #
-# Install default config files on target image
+# Install docker-compose
 #
+
+# install docker-compose
+sudo apt-get install -y libffi-dev libssl-dev  python3 python3-pip
+sudo pip3 install docker-compose
+
 
 # Setup docker-compose config for boot service 
 install -m 644 files/docker-compose-app.service "${ROOTFS_DIR}/etc/systemd/system/docker-compose-app.service"
