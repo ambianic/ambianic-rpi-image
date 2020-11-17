3f507e8f80ac9717981dea474a663e3372a74d87
Ivelin Ivanov <ivelin@ambianic.ai>
2020-11-17 13:40:53 -0600

    fix: install docker-compose.yaml on target env

--- pi-gen-stage-ambianic/01-docker/02-run.sh
+++ pi-gen-stage-ambianic/01-docker/02-run.sh
@@ -12,8 +12,13 @@ sudo apt install -y libffi-dev libssl-dev  python3 python3-pip
 sudo pip3 install docker-compose
 
 
-# Setup docker-compose config for boot service 
+# Setup docker-compose as a boot service 
 install -m 644 files/docker-compose-app.service "${ROOTFS_DIR}/etc/systemd/system/docker-compose-app.service"
+
+# Install default docker-compose config
+install -m 644 files/docker-compose.yaml "${ROOTFS_DIR}/opt/ambianic/docker-compose.yaml"
+
+
 
 # enable docker compose on boot
 systemctl enable docker-compose-app
