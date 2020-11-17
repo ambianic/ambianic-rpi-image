aa3b1e7fbf32f61d56838dc29a495d52bebdb6b5
Ivelin Ivanov <ivelin@ambianic.ai>
2020-11-17 14:48:23 -0600

    fix: chroot sandbox

--- pi-gen-stage-ambianic/01-docker/02-run.sh
+++ pi-gen-stage-ambianic/01-docker/02-run.sh
@@ -7,16 +7,22 @@
 # set fail quick, verbose output
 set +exu
 
-# install docker-compose
-sudo apt install -y libffi-dev libssl-dev  python3 python3-pip
-sudo pip3 install docker-compose
-
-
 # Setup docker-compose as a boot service 
 install -m 644 files/docker-compose-app.service "${ROOTFS_DIR}/etc/systemd/system/docker-compose-app.service"
 
 # Install default docker-compose config
 install -m 644 files/docker-compose.yaml "${ROOTFS_DIR}/opt/ambianic/docker-compose.yaml"
 
+on_chroot << EOF
+
+# set fail quick, verbose output
+set +exu
+
+# install docker-compose
+sudo apt install -y libffi-dev libssl-dev  python3 python3-pip
+sudo pip3 install docker-compose
+
 # enable docker compose on boot
 systemctl enable docker-compose-app
+
+EOF
