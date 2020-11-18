9eb71f81fb3d934e508559033efda4316ed43ccd
Ivelin Ivanov <ivelin@ambianic.ai>
2020-11-18 01:21:32 -0600

    fix: optimize install [skip ci]

--- pi-gen-stage-ambianic/01-docker/01-run.sh
+++ pi-gen-stage-ambianic/01-docker/01-run.sh
@@ -4,36 +4,24 @@
 # Install docker engine and ambianic edge images
 #
 
-# make cpuinfo visible to docker within chroot
-set +exu
-echo "CPU Info: /proc/cpuinfo"
-cat /proc/cpuinfo
-# make cpuinfo visible to chroot processes
-# mount -o bind /proc "${ROOTFS_DIR}/proc"
-
 # Switch to the Raspberry OS image space and run the following script in it
 on_chroot << EOF
 # set bash flags: e - fail on unset vars, x - verbose, u - fail quick 
-echo "Executing in chroot space: ${ROOTFS_DIR}"
 set -exu
 
-# Install docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-# echo "\$USER"
-# enable current user to run docker
-# sudo usermod -aG docker "\${USER}"
-# enable docker on boot
-sudo systemctl enable docker
-
-# can't use systemctl in chroot
-# sudo systemctl start docker
-# start docker daemon
-# sudo dockerd -H unix:///var/run/docker.sock
-# Unable to run docker in github action > chroot > qemu environment 
-# docker version
-# docker info
-#docker run hello-world
- 
+echo "Executing in chroot space: ${ROOTFS_DIR}"
+INSTALLDIR="/opt/ambianic"
+echo "\$INSTALLDIR"
+mkdir -p "\$INSTALLDIR"
+BRANCH=rpi-image
+sudo true
+if ! type "git" > /dev/null; then
+    sudo apt update -q && sudo apt install git -y
+fi
+echo "Installing Ambianic.ai in \$INSTALLDIR"
+# clean install using ambianic quickstart scripts
+echo "Installing in \$INSTALLDIR"
+git clone -b "\$BRANCH" "https://github.com/ambianic/ambianic-quickstart.git" "\$INSTALLDIR"
+sh "\$INSTALLDIR/scripts/setup.sh"
 EOF
 
