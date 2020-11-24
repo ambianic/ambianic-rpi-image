1ba6c3dd62bb962ece3134227ce71b5f96c73a9f
Ivelin Ivanov <ivelin@ambianic.ai>
2020-11-24 16:16:09 -0600

    fix: switch to quickstart rpi-image branch
    Until the quickstart branch is merged into master

--- pi-gen-stage-ambianic/01-docker/01-run.sh
+++ pi-gen-stage-ambianic/01-docker/01-run.sh
@@ -17,7 +17,8 @@ FIRST_USER_NAME="${FIRST_USER_NAME}"
 INSTALLDIR="/opt/ambianic"
 echo "\$INSTALLDIR"
 mkdir -p "\$INSTALLDIR"
-BRANCH=master
+# BRANCH=master
+BRANCH=rpi-image
 sudo true
 if ! type "git" > /dev/null; then
     sudo apt update -q && sudo apt install git -y
