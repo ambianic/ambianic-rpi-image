eabea7fee5132b31a86f29bb5296b1769c54f0ab
Ivelin Ivanov <ivelin@ambianic.ai>
2020-11-24 21:15:04 -0600

    fix: exec context

--- pi-gen-stage-ambianic/01-docker/02-run.sh
+++ pi-gen-stage-ambianic/01-docker/02-run.sh
@@ -4,12 +4,9 @@
 # Enable picam access for docker and ambianic user
 #
 
-# Switch to the Raspberry OS image space and run the following script in it
-on_chroot << EOF
 # set bash flags: e - fail on unset vars, x - verbose, u - fail quick 
 set -exu
 # grant all users accesss to video group
 # See more here: https://www.losant.com/blog/how-to-access-the-raspberry-pi-camera-in-docker
-sudo install -m 644 -o "$FIRST_USER_NAME" -g "$FIRST_USER_NAME" "files/99-camera.rules" "/etc/udev/rules.d/99-camera.rules"
-EOF
+sudo install -m 644 -o "$FIRST_USER_NAME" -g "$FIRST_USER_NAME" "files/99-camera.rules" "${ROOTFS_DIR}/etc/udev/rules.d/99-camera.rules"
 
