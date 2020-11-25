b02cea660782026d9e6a865126166e034f2aa7ba
Ivelin Ivanov <ivelin@ambianic.ai>
2020-11-24 21:56:04 -0600

    fix: ownership flags

--- pi-gen-stage-ambianic/01-docker/02-run.sh
+++ pi-gen-stage-ambianic/01-docker/02-run.sh
@@ -6,7 +6,10 @@
 
 # set bash flags: e - fail on unset vars, x - verbose, u - fail quick 
 set -exu
+
 # grant all users accesss to video group
 # See more here: https://www.losant.com/blog/how-to-access-the-raspberry-pi-camera-in-docker
-sudo install -m 644 -o "$FIRST_USER_NAME" -g "$FIRST_USER_NAME" "files/99-camera.rules" "${ROOTFS_DIR}/etc/udev/rules.d/99-camera.rules"
+
+# copy system rules file to target image dir
+sudo install -m 644 "files/99-camera.rules" "${ROOTFS_DIR}/etc/udev/rules.d/99-camera.rules"
 
